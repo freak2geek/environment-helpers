81313660703a049c2428cfde19e8c68502a70379
Nacho Codoñer <igcogi@gmail.com>
2018-12-10 01:48:50 +0100

    chore(meteor-m): implement helpers for meteor m

---
+++ src/meteor-m.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+source "./src/constants.sh"
+source "./src/meteor.sh"
+
+function hasMeteorM() {
+    meteor npm ls --depth 0 -g 2>/dev/null | grep -icq " m@"
+}
+
+function installMeteorM() {
+    printf "${BLUE}[-] Installing meteor m...${NC}\n"
+    meteor npm install m -g
+}
+
+function uninstallMeteorM() {
+    printf "${BLUE}[-] Uninstalling meteor m...${NC}\n"
+    meteor npm uninstall m -g
+}
+
+function checkMeteorM() {
+    if hasMeteorM; then
+        printf "${GREEN}[✔] meteor m${NC}\n"
+    else
+        printf "${RED}[x] meteor m${NC}\n"
+    fi
+}
+
+function setupMeteorM() {
+    if ! hasMeteor; then
+        setupMeteor
+    fi
+
+    if hasMeteorM; then
+        printf "${GREEN}[✔] Already meteor m${NC}\n"
+        return
+    fi
+
+    installMeteorM
+}
+
+function purgeMeteorM() {
+    if ! hasMeteorM; then
+        return
+    fi
+
+    uninstallMeteorM
+}
