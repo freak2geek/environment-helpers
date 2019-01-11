bf8da3d7dd9591db7014d169891772000efa893b
Nacho Codoñer <igcogi@gmail.com>
2019-01-11 20:56:38 +0100

    chore(meteor-lerna): implement helpers

--- dist/index.sh
+++ dist/index.sh
@@ -324,6 +324,52 @@ function endsWithNewLine() {
 
 
 
+function hasMeteorLerna() {
+    hasMeteor && find ${METEOR_TOOL_DIR} -type d -name "lerna" | grep -icq "lerna"
+}
+
+function installMeteorLerna() {
+    printf "${BLUE}[-] Installing meteor lerna...${NC}\n"
+    meteor npm install lerna -g
+}
+
+function uninstallMeteorLerna() {
+    printf "${BLUE}[-] Uninstalling meteor lerna...${NC}\n"
+    meteor npm uninstall lerna -g
+}
+
+function checkMeteorLerna() {
+    if hasMeteorLerna; then
+        printf "${GREEN}[✔] meteor lerna${NC}\n"
+    else
+        printf "${RED}[x] meteor lerna${NC}\n"
+    fi
+}
+
+function setupMeteorLerna() {
+    if ! hasMeteor; then
+        setupMeteor
+    fi
+
+    if hasMeteorLerna; then
+        printf "${GREEN}[✔] Already meteor lerna${NC}\n"
+        return
+    fi
+
+    if ! hasMeteorLerna; then
+        installMeteorLerna
+    fi
+}
+
+function purgeMeteorLerna() {
+    if ! hasMeteorLerna; then
+        return
+    fi
+
+    uninstallMeteorLerna
+}
+
+
 function hasMeteorM() {
    hasMeteor && find ${METEOR_TOOL_DIR} -type d -name "m" | grep -icq "m"
 }
