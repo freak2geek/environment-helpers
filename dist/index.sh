5ab2217b3d2b91cf457839a3ad42be198f565f68
Nacho Codoñer <igcogi@gmail.com>
2019-02-10 14:41:31 +0100

    chore: correct reference to the oldPwd ref

--- dist/index.sh
+++ dist/index.sh
@@ -536,8 +536,8 @@ function configEnvrc() {
 
     if ! hasLocalHomeAlias; then
         tryPrintNewLine ~/.envrc
-        echo "alias @${PROJECT_NAME}=\"_${PROJECT_NAME}() { export OLD_PWD=\${PWD}; cd \${${localHomeName}} }; _${PROJECT_NAME}\"" >>~/.envrc
-        echo "alias @old-pwd=\"_oldpwd() { cd \${OLD_PWD} }; _oldpwd\"" >>~/.envrc
+        echo "alias @${PROJECT_NAME}=\"_${PROJECT_NAME}() { export OLDPWD=\${PWD}; cd \${${localHomeName}} }; _${PROJECT_NAME}\"" >>~/.envrc
+        echo "alias @old-pwd=\"_oldpwd() { cd \${OLDPWD} }; _oldpwd\"" >>~/.envrc
         loadEnvrc
         printf "${GREEN}[✔] local home alias${NC}\n"
     fi
