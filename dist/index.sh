da845614aea9ff6e799fcd23a0d9c072cab4bb39
Nacho Codoñer <igcogi@gmail.com>
2019-02-18 21:50:02 +0100

    chore: update version

--- dist/index.sh
+++ dist/index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# @freak2geek/scripts - 1.1.3
+# @freak2geek/scripts - 1.1.4
 
 ENVRC_DYNAMIC_LOADER="$(curl -s https://raw.githubusercontent.com/freak2geek/environment-helpers/master/helpers/envrc-dynamic-loader.sh)"
 
