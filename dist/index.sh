32fe068a09a66ff83e4b59de8c3cdd1b676f9787
Nacho Codoñer <igcogi@gmail.com>
2019-03-01 06:45:18 +0100

    chore: pre-check "package.json" existence

--- dist/index.sh
+++ dist/index.sh
@@ -1554,12 +1554,12 @@ function cleanMeteorApp() {
 
 function getNpmPackageName() {
     packagePath=${1-'package.json'}
-    cat ${packagePath} | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'
+    [[ -f ${packagePath} ]] && cat ${packagePath} | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'
 }
 
 function getNpmPackageVersion() {
     packagePath=${1-'package.json'}
-    cat ${packagePath} | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'
+    [[ -f ${packagePath} ]] && cat ${packagePath} | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'
 }
 
 
