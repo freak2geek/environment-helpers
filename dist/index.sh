593274ec817936cfccef4ead89437e738b95d957
Nacho Codoñer <igcogi@gmail.com>
2019-03-21 07:28:05 +0100

    chore: implement helpers to install xcode

--- dist/index.sh
+++ dist/index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# @freak2geek/scripts - 1.3.6
+# @freak2geek/scripts - 1.4.0
 
 
 
@@ -766,6 +766,67 @@ function setupIfconfig() {
     installIfconfig
 }
 
+
+
+function hasXcode() {
+    isOSX && [[ "$(xcode-select -p | grep -ic "not found")" -eq "0" ]] && [[ -d /Applications/Xcode.app ]]
+}
+
+function installXcode() {
+    printf "${BLUE}[-] Installing xcode...${NC}\n"
+    brew install mas
+    mas install 497799835
+    xcode-select --install
+}
+
+function configXcode() {
+    printf "${BLUE}[-] Configuring xcode...${NC}\n"
+    sudo xcodebuild -license accept
+}
+
+function uninstallXcode() {
+    printf "${BLUE}[-] Uninstalling xcode...${NC}\n"
+    sudo rm -rf /Applications/Xcode.app
+    sudo rm -rf /Library/Developer/CommandLineTools
+    sudo rm -rf ~/Library/Caches/com.apple.dt.Xcode
+    sudo rm -rf ~/Library/Developer
+    sudo rm -f ~/Library/MobileDevice
+    sudo rm -f ~/Library/Preferences/com.apple.dt.Xcode.plist
+    sudo rm -f /Library/Preferences/com.apple.dt.Xcode.plist
+    sudo rm -f /System/Library/Receipts/com.apple.pkg.XcodeExtensionSupport.bom
+    sudo rm -f /System/Library/Receipts/com.apple.pkg.XcodeExtensionSupport.plist
+}
+
+function checkXcode() {
+    if hasXcode; then
+        printf "${GREEN}[✔] xcode${NC}\n"
+    else
+        printf "${RED}[x] xcode${NC}\n"
+    fi
+}
+
+function setupXcode() {
+    if hasXcode; then
+        printf "${GREEN}[✔] Already xcode${NC}\n"
+        return
+    fi
+
+    installXcode
+
+    configXcode
+}
+
+function purgeXcode() {
+    if ! isOSX; then
+        return
+    fi
+
+    if ! hasXcode; then
+        return
+    fi
+
+    uninstallXcode
+}
 
 
 function hasMeteorConcurrently() {
---
+++ src/ios.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+source "./src/constants.sh"
+source "./src/helpers.sh"
+
+function hasXcode() {
+    isOSX && [[ "$(xcode-select -p | grep -ic "not found")" -eq "0" ]] && [[ -d /Applications/Xcode.app ]]
+}
+
+function installXcode() {
+    printf "${BLUE}[-] Installing xcode...${NC}\n"
+    brew install mas
+    mas install 497799835
+    xcode-select --install
+}
+
+function configXcode() {
+    printf "${BLUE}[-] Configuring xcode...${NC}\n"
+    sudo xcodebuild -license accept
+}
+
+function uninstallXcode() {
+    printf "${BLUE}[-] Uninstalling xcode...${NC}\n"
+    sudo rm -rf /Applications/Xcode.app
+    sudo rm -rf /Library/Developer/CommandLineTools
+    sudo rm -rf ~/Library/Caches/com.apple.dt.Xcode
+    sudo rm -rf ~/Library/Developer
+    sudo rm -f ~/Library/MobileDevice
+    sudo rm -f ~/Library/Preferences/com.apple.dt.Xcode.plist
+    sudo rm -f /Library/Preferences/com.apple.dt.Xcode.plist
+    sudo rm -f /System/Library/Receipts/com.apple.pkg.XcodeExtensionSupport.bom
+    sudo rm -f /System/Library/Receipts/com.apple.pkg.XcodeExtensionSupport.plist
+}
+
+function checkXcode() {
+    if hasXcode; then
+        printf "${GREEN}[✔] xcode${NC}\n"
+    else
+        printf "${RED}[x] xcode${NC}\n"
+    fi
+}
+
+function setupXcode() {
+    if hasXcode; then
+        printf "${GREEN}[✔] Already xcode${NC}\n"
+        return
+    fi
+
+    installXcode
+
+    configXcode
+}
+
+function purgeXcode() {
+    if ! isOSX; then
+        return
+    fi
+
+    if ! hasXcode; then
+        return
+    fi
+
+    uninstallXcode
+}
