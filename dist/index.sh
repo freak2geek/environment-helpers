1b3d8ffa6871dacdd99f014bab9e0904f782783f
Nacho Codoñer <igcogi@gmail.com>
2019-05-04 20:21:57 +0200

    chore: implement helpers for docker

--- dist/index.sh
+++ dist/index.sh
@@ -577,22 +577,41 @@ function purgeDnsmasq() {
 }
 
 
-function hasDocker() {
-    which docker >/dev/null && [[ "$(which docker | grep -ic "not found")" -eq "0" ]]
+function hasDockerInMac() {
+    [[ "$(brew cask list 2>&1 | grep -ic "docker")" -ne "0" ]]
 }
 
-function installDocker() {
+function hasDockerInLinux() {
+    [[ "$(snap list docker 2>&1 | grep -ic "no matching snaps")" -eq "0" ]]
+}
+
+function installDockerInMac() {
     printf "${BLUE}[-] Installing docker...${NC}\n"
     brew install docker
+    brew install docker-compose
+    brew cask install docker
 }
 
-function uninstallDocker() {
+function installDockerInLinux() {
+    printf "${BLUE}[-] Installing docker...${NC}\n"
+    yes | sudo snap install docker
+}
+
+function uninstallDockerInMac() {
     printf "${BLUE}[-] Uninstalling docker...${NC}\n"
-    brew uninstall docker
+    brew uninstall docker docker-compose
+    brew cask uninstall docker
+}
+
+function uninstallDockerInLinux() {
+    printf "${BLUE}[-] Uninstalling docker...${NC}\n"
+    yes | sudo snap remove docker
 }
 
 function checkDocker() {
-    if hasDocker; then
+    if isOSX && hasDockerInMac; then
+        printf "${GREEN}[✔] docker${NC}\n"
+    elif isLinux && hasDockerInLinux; then
         printf "${GREEN}[✔] docker${NC}\n"
     else
         printf "${RED}[x] docker${NC}\n"
@@ -600,21 +619,78 @@ function checkDocker() {
 }
 
 function setupDocker() {
-    if hasDocker; then
+    if isOSX && hasDockerInMac; then
+        printf "${GREEN}[✔] Already docker${NC}\n"
+        return
+    elif isLinux && hasDockerInLinux; then
         printf "${GREEN}[✔] Already docker${NC}\n"
         return
     fi
 
-    installDocker
+    printf "${BLUE}[-] Setting up docker...${NC}\n"
+
+    if isOSX && ! hasDockerInMac; then
+        installDockerInMac
+    elif isLinux && ! hasDockerInLinux; then
+        installDockerInLinux
+    fi
 }
 
 function purgeDocker() {
-    if ! hasDocker; then
+    if isOSX && ! hasDockerInMac; then
+        return
+    elif isLinux && ! hasDockerInLinux; then
         return
     fi
 
-    uninstallDocker
+    printf "${BLUE}[-] Purging docker...${NC}\n"
+
+    if isOSX; then
+        uninstallDockerInMac
+    elif isLinux; then
+        uninstallDockerInLinux
+    fi
 }
+
+function isRunningDocker() {
+    [[ "$(docker stats --no-stream 2>&1 | grep -ic "Cannot connect to the Docker daemon")" -eq "0" ]]
+}
+
+function waitDocker() {
+    while ! isRunningDocker; do sleep 1; done
+}
+
+function startDockerInMac() {
+    open --background /Applications/Docker.app
+}
+
+function startDockerInLinux() {
+    snap run docker
+}
+
+function startDocker() {
+    if isRunningDocker; then
+        printf "${GREEN}[✔] Already running docker${NC}\n"
+        return
+    fi
+
+    printf "${BLUE}[-] Starting docker...${NC}\n"
+    if isOSX; then
+        startDockerInMac
+    elif isLinux; then
+        startDockerInLinux
+    fi
+}
+
+function killDocker() {
+    if ! isRunningDocker; then
+        return
+    fi
+
+    printf "${BLUE}[-] Killing docker...${NC}\n"
+    sudo pkill docker
+}
+
 
 
 # GIT FLOW default config
